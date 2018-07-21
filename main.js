87c4939dbabba2e6bda3c5bee70fc2e5434d2be2
JatavienBarnard <jatavien.barnard@gmail.com>
2018-07-20 19:12:23 -0700

    Movement is completed with buttons

--- main.js
+++ main.js
@@ -1,18 +1,63 @@
 $(document).ready(function(){
     
-    $("#player").click(function(){
-        $("h1").hide();
-        movePlayer(50,0);
+    $("#left").click(function(){
+        leftPlayer(50,0);
     });
+
+    $("#up").click(function(){
+        upPlayer(50);
+    });
+
+    $("#right").click(function(){
+        rightPlayer(50);
+    });
+
+    $("#down").click(function(){
+        downPlayer(50);
+    });
+
 });
 
-function movePlayer(dx, dy)
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+async function leftPlayer(dx)
 {
     for(let i = 0; i < dx; i++)
     {
-        setTimeout(function(){
+            $("#player").css("cx", "-=1px");
+            await sleep(15);
+    }
+    
+}
+
+async function rightPlayer(dx)
+{
+    for(let i = 0; i < dx; i++)
+    {
+            $("#player").css("cx", "+=1px");
+            await sleep(15);
+    }
+    
+}
+
+async function upPlayer(dy)
+{
+    for(let i = 0; i < dy; i++)
+    {
+            $("#player").css("cy", "-=1px");
+            await sleep(15);
+    }
+    
+}
+
+async function downPlayer(dy)
+{
+    for(let i = 0; i < dy; i++)
+    {
             $("#player").css("cy", "+=1px");
-        }, 500);
+            await sleep(15);
     }
     
 }
